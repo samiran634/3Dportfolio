33398007da8afa31371c2b9e02f50a59a03f6217
samiran634 <samiranchakraborty2006@gmail.com>
2025-06-25 23:22:27 +0530

    new commit

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -51,7 +51,7 @@ const Hero = () => {
             </div>
 
             <p className="text-white-50 md:text-xl relative z-10 pointer-events-none">
-              Hi, I’m Sam, a developer from India with a passion for
+              Hi, This is Sam, a developer from India with a passion for
               code.
             </p>
 
